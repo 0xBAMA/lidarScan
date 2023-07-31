01b3b5633199dd91f5235dda9d840f45e3ac4e32
jb <jon.baker1222@gmail.com>
2023-07-31 09:48:56 -0500

    add note on CloudCompare

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 # pip install pylas
 # pip install laszip - for handling .laz
 
+# using laz does not seem to be functional - can't figure out how to set it up the way it expects
+# CloudCompare seems to be good software for doing the conversion: https://www.danielgm.net/cc/forum/viewtopic.php?t=4371
+
 import pylas
 
 las = pylas.read( "USGS_LPC_TX_Pecos_Dallas_2018_D19_14SPB5768.laz" )
